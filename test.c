7b87e87f7aa749daf55ab283ebeae4bf7ec0c6aa
Poojatekale1234 <124156137+Poojatekale1234@users.noreply.github.com>
2024-10-07 09:43:18 +0530

    Update test.c

--- test.c
+++ test.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 int main() {
    // printf() displays the string inside quotation
-   printf("Hello, World!");
+   printf("\n This is my first github code and executing it google cloud console \n");
    return 0;
 }
